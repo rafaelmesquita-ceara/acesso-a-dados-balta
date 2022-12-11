6f3b19099847b6d59be0571d931a177e8bf670fe
Rafael <rafaelempresarialacc@gmail.com>
2022-12-11 19:08:03 -0300

    Utilizando acesso à dados com ADO.NET

---
+++ Projeto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=BaltaPopulado;User ID=sa;Password=;TrustServerCertificate=True";
+            // Microsoft.Data.SqlClient
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado");
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader(); // Retorna registros
+                                                          //command.ExecuteNonReader(); Retorna quantidade de linhas afetadas
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # acesso-a-dados-balta
+
+## Ferramentas utilizadas
+- Docker
+- SQL Server
+- Azure Data Studio
+- dotnet sdk 7.0
+- Visual Studio Code
+
+### ADO.NET
+    Core de acesso à dados para N provedores que a microsoft suporta
