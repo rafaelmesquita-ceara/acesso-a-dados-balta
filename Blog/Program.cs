cd40f7ec2397ea8320896a90129005a0013e0ebb
Rafael <rafaelempresarialacc@gmail.com>
2022-12-19 20:47:59 -0300

    Mão na massa - Criação de repositories e models

---
+++ Blog/Program.cs
@@ -0,0 +1,81 @@
+﻿using Blog.Models;
+using Blog.Repositories;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = "Server=localhost,1433;Database=Blog;User ID=sa;Password=Ericsouza.123;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(CONNECTION_STRING);
+            connection.Open();
+            //ReadUsers(connection);
+            //ReadUser(connection);
+            //CreateUser(connection);
+            //UpdateUser(connection);
+            //DeleteUser(connection);
+            connection.Close();
+        }
+
+        public static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+
+            foreach (var user in users)
+                Console.WriteLine(user.Name);
+        }
+        public static void ReadUser(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var user = repository.Get(1);
+            Console.WriteLine(user.Name);
+        }
+
+        public static void CreateUser(SqlConnection connection)
+        {
+            var user = new User()
+            {
+                Bio = "Equipe Balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            var repository = new UserRepository(connection);
+            repository.Create(user);
+            Console.WriteLine("Cadastro realizado com sucesso");
+        }
+
+        public static void UpdateUser(SqlConnection connection)
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Equipe | Balta.io",
+                Email = "hello@balta.io",
+                Image = "https://...",
+                Name = "Equipe de suporte balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            var repository = new UserRepository(connection);
+            repository.Create(user);
+            Console.WriteLine("Atualização realizada com sucesso");
+        }
+
+        public static void DeleteUser(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var user = repository.Get(1);
+            repository.Create(user);
+            Console.WriteLine("Exclusão realizada com sucesso");
+        }
+    }
+}
